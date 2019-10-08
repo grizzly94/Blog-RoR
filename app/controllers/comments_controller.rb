96e5caa8b6f13f5e91e73cae85368bea7669b4c4
Alejandro Ramírez Embry <alejandro.ramirez.e@usach.cl>
2019-10-08 00:19:52 -0400

    ADD: AJAX para los comentarios
    Se agregan sin tener que recargar y se limpia el campo de texto

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,8 +13,8 @@ class CommentsController < ApplicationController
     @comment.article = @article
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment.article, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment.article }
+        #format.html { redirect_to @comment.article, notice: 'Comment was successfully created.' }
+        #format.json { render :show, status: :created, location: @comment.article }
         format.js
       else
         format.html { render :new }
